087990845107ae11d0d7c3f01188704d13a30b4e
halilkrkn <Halilkrkn000@gmail.com>
2025-02-16 18:28:31 +0300

    JAXB and Marshaller with Web Client for Soap Request

--- SmsMultiSenderLib/src/main/java/org/halilkrkn/smsmultisenderlib/SmsMultiSenderLibApplication.java
+++ SmsMultiSenderLib/src/main/java/org/halilkrkn/smsmultisenderlib/SmsMultiSenderLibApplication.java
@@ -1,9 +1,12 @@
 package org.halilkrkn.smsmultisenderlib;
 
+import org.halilkrkn.smsmultisenderlib.config.SmsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(SmsProperties.class)
 public class SmsMultiSenderLibApplication {
 
 	public static void main(String[] args) {
