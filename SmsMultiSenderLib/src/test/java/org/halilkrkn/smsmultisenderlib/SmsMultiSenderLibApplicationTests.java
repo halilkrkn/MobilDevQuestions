dca0b8be1397f4cb6c8c48023b356d10e1f8e70f
halilkrkn <Halilkrkn000@gmail.com>
2025-02-16 18:17:54 +0300

    JAXB and Marshaller with Web Client for Soap Request

---
+++ SmsMultiSenderLib/src/test/java/org/halilkrkn/smsmultisenderlib/SmsMultiSenderLibApplicationTests.java
@@ -0,0 +1,13 @@
+package org.halilkrkn.smsmultisenderlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsMultiSenderLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
